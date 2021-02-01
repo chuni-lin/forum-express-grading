23ef5bd1786a075528290bb7c1085e438c9d9674
chuni-lin <william811566@gmail.com>
2021-02-01 15:17:09 +0800

    feat: Confirm user password and unique email

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 const db = require('./models')
 const app = express()
 const port = 3000
@@ -7,6 +9,14 @@ const port = 3000
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
