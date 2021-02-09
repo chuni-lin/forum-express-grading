f5582ded82a25e8f535e50f0004b118f322c379c
chuni-lin <william811566@gmail.com>
2021-02-09 17:26:57 +0800

    feat: Add restaurant dashboard

--- routes/index.js
+++ routes/index.js
@@ -59,6 +59,7 @@ module.exports = (app, passport) => {
   // rest
   app.get('/restaurants/feeds', authenticated, restController.getFeeds)
   app.get('/restaurants/:id', authenticated, restController.getRestaurant)
+  app.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 
   // comment
   app.post('/comments', authenticated, commentController.postComment)
