fc5edc39703cc5a575d8ec66fe6343cee9d80f00
chuni-lin <william811566@gmail.com>
2021-02-17 17:30:23 +0800

    refactor: getRestaurant and getCategories

--- controllers/api/adminController.js
+++ controllers/api/adminController.js
@@ -5,9 +5,11 @@ const adminService = require('../../services/adminService')
 
 const adminController = {
   getRestaurants: (req, res) => {
-    adminService.getRestaurants(req, res, (data) => {
-      return res.json(data)
-    })
-  }
+    adminService.getRestaurants(req, res, (data) => res.json(data))
+  },
+
+  getRestaurant: (req, res) => {
+    adminService.getRestaurant(req, res, (data) => res.json(data))
+  },
 }
 module.exports = adminController
--- services/adminService.js
+++ services/adminService.js
@@ -6,7 +6,13 @@ const adminService = {
   getRestaurants: (req, res, callback) => {
     return Restaurant.findAll({ raw: true, nest: true, include: [Category] })
       .then(restaurants => callback({ restaurants }))
-  }
+  },
+
+  getRestaurant: (req, res, callback) => {
+    Restaurant.findByPk(req.params.id, { include: [Category] }).then(restaurant =>
+      callback({ restaurant: restaurant.toJSON() })
+    )
+  },
 }
 
 module.exports = adminService
